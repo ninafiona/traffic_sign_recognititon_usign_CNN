65da28f0ae38236a20eac4b3d2db6cc10f6c0815
Nina Grabka <nina.grabka@dive-sph.de>
2019-06-13 15:26:56 +0200

    view images

--- src/show_images.py
+++ src/show_images.py
@@ -2,14 +2,21 @@ import cv2
 import numpy as np
 
 path_detect = "C:/Users/ninaf/Desktop/TrainIJCNN2013/"
-path_class = "C:/Users/ninaf/Desktop/GTSRB/Final_Training/Images/"
+path_class = "C:/Users/ninaf/Desktop/GTSRB/Final_Training/Images/00000/"
 
-# Read ground truth file
-gt_data = np.genfromtxt(path_detect + "gt.txt", delimiter = ";", dtype=None)
+# Read ground truth file of d(etection) and c(lassification)
+gt_data_d = np.genfromtxt(path_detect + "gt.txt", delimiter = ";", dtype=None)
+gt_data_c = np.genfromtxt(path_class + "GT-00000.csv", delimiter = ";", dtype=None, skip_header=1)
 
 # Read and show images
-for i_image in range(len(gt_data)):
-    img = cv2.imread(path_detect + gt_data[i_image][0].decode('utf-8'))
+# for i_image in range(len(gt_data_d)):
+#     img = cv2.imread(path_detect + gt_data_d[i_image][0].decode('utf-8'))
+#     cv2.namedWindow("Images", cv2.WND_PROP_FULLSCREEN)
+#     cv2.imshow( "Images", img )
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+for i_image in range(len(gt_data_c)):
+    img = cv2.imread(path_class + gt_data_c[i_image][0].decode('utf-8'))
     cv2.namedWindow("Images", cv2.WND_PROP_FULLSCREEN)
     cv2.imshow( "Images", img )
     cv2.waitKey(0)
