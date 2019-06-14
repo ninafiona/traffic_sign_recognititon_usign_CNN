498c7823f296599c043b5bf8844be98d6d09e5e3
Nina Grabka <nina.grabka@dive-sph.de>
2019-06-14 15:00:00 +0200

    inception network added

--- src/classify_20_and_30.py
+++ src/classify_20_and_30.py
@@ -1,16 +1,31 @@
 import cv2
 import numpy as np
 
-path_detect = "C:/Users/ninaf/Desktop/TrainIJCNN2013/"
 path_class = "C:/Users/ninaf/Desktop/GTSRB/Final_Training/Images/"
 
-# Read ground truth file
-gt_data = np.genfromtxt(path_detect + "gt.txt", delimiter = ";", dtype=None)
+# Read ground truth file of c(lassification)
+gt_data_20 = np.genfromtxt(path_class + "00000/" + "GT-00000.csv", delimiter = ";", dtype=None, skip_header=1)
+gt_data_trucks = np.genfromtxt(path_class + "00010/" + "GT-00010.csv", delimiter = ";", dtype=None, skip_header=1)
 
-# Read and show images
-for i_image in range(len(gt_data)):
-    img = cv2.imread(path_detect + gt_data[i_image][0].decode('utf-8'))
+# save images signs 20 (class 0) and trucks (class 10) to lists
+images_20 = []
+for i_image in range(len(gt_data_20)):
+    img = cv2.imread(path_class + "00000/" + gt_data_20[i_image][0].decode('utf-8'))
+    images_20.append(img)
+images_trucks = []
+for i_image in range(len(gt_data_trucks)):
+    img = cv2.imread(path_class + "00010/" + gt_data_trucks[i_image][0].decode('utf-8'))
+    images_trucks.append(img)
+
+# show all images 
+for i_image in images_trucks:
     cv2.namedWindow("Images", cv2.WND_PROP_FULLSCREEN)
-    cv2.imshow( "Images", img )
+    cv2.imshow( "Images", i_image )
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+for i_image in images_20:
+    cv2.namedWindow("Images", cv2.WND_PROP_FULLSCREEN)
+    cv2.imshow( "Images", i_image )
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
---
+++ src/ppm_to_jpg.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import cv2
+import numpy as np
+import os
+os.path.splitext('/home/user/somefile.txt')[0]+'.jpg'
+
+path_class = "C:/Users/ninaf/Desktop/GTSRB/Final_Training/Images/"
+
+# Read ground truth file of c(lassification)
+gt_data_20 = np.genfromtxt(path_class + "00000/" + "GT-00000.csv", delimiter = ";", dtype=None, skip_header=1)
+gt_data_trucks = np.genfromtxt(path_class + "00010/" + "GT-00010.csv", delimiter = ";", dtype=None, skip_header=1)
+
+# save images signs 20 (class 0) and trucks (class 10) to lists
+images_20 = []
+for i_image in range(len(gt_data_20)):
+    img = cv2.imread(path_class + "00000/" + gt_data_20[i_image][0].decode('utf-8'))
+    images_20.append(img)
+    im = Image.open(path_class + "00000/" + gt_data_20[i_image][0].decode('utf-8'))
+    im.save(os.path.splitext(path_class + "00000_jpg/" + gt_data_20[i_image][0].decode('utf-8'))[0]+'.jpg')
+images_trucks = []
+for i_image in range(len(gt_data_trucks)):
+    img = cv2.imread(path_class + "00010/" + gt_data_trucks[i_image][0].decode('utf-8'))
+    images_trucks.append(img)
+    im = Image.open(path_class + "00010/" + gt_data_trucks[i_image][0].decode('utf-8'))
+    im.save(os.path.splitext(path_class + "00010_jpg/" + gt_data_trucks[i_image][0].decode('utf-8'))[0]+'.jpg')
